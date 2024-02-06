6f924a0d498a233147f1c07f4c928b063dabe44b
II <46581451+yuqing-yang@users.noreply.github.com>
2024-02-06 03:12:06 +0000

    copilot second commit

---
+++ member.js
@@ -0,0 +1,15 @@
+function skillsMember() {
+  return {
+    "skills": [
+      "HTML",
+      "CSS",
+      "JS",
+      "React",
+      "Node",
+      "Express",
+      "MongoDB",
+      "Git",
+      "GitHub"
+    ]
+  }
+}
