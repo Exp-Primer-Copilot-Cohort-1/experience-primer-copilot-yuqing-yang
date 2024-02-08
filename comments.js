edec0adfc99d0137ba0a104ba225f48bdcaf71ca
II <46581451+yuqing-yang@users.noreply.github.com>
2024-02-08 02:19:07 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,42 +1,22 @@
 // creat web server
-// 1. load http module
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-var qs = require('querystring');
-var comments = [];
-
-// 2. create server
-http.createServer(function(request, response){
-    var url_parts = url.parse(request.url);
-    if(url_parts.pathname == '/comment'){
-        if(request.method == 'POST'){
-            var body = '';
-            request.on('data', function(chunk){
-                body += chunk;
-            });
-            request.on('end', function(){
-                var post = qs.parse(body);
-                comments.push(post.comment);
-                response.end('success');
-            });
-        }
-        else{
-            var all = '';
-            comments.forEach(function(comment){
-                all += comment + '<br>';
-            });
-            response.end(all);
-        }
-    }
-    else if(url_parts.pathname == '/'){
-        fs.readFile('./index.html', function(error, data){
-            response.end(data);
-        });
-    }
-    else{
-        response.end('404');
-    }
-}).listen(3000);
-
-console.log('Server running at http://
+// use express
+// use body parser
+// use cookie parser
+// use express-session
+// use serve-favicon
+// use morgan
+// use method-override
+// use connect-flash
+// use multer
+// use mongoose
+// use passport
+// use passport-local
+// use passport-facebook
+// use passport-twitter
+// use passport-google-oauth
+// use passport-github
+// use passport-linkedin
+// use passport-instagram
+// use passport-local-mongoose
+// use express-validator
+// use express-messages
