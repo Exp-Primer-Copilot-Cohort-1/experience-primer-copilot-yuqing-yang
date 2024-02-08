598bee4ed84aff0d1929d83d4efc7f45986d3c0b
II <46581451+yuqing-yang@users.noreply.github.com>
2024-02-08 02:22:17 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,22 +1,28 @@
-// creat web server
-// use express
-// use body parser
-// use cookie parser
-// use express-session
-// use serve-favicon
-// use morgan
-// use method-override
-// use connect-flash
-// use multer
-// use mongoose
-// use passport
-// use passport-local
-// use passport-facebook
-// use passport-twitter
-// use passport-google-oauth
-// use passport-github
-// use passport-linkedin
-// use passport-instagram
-// use passport-local-mongoose
-// use express-validator
-// use express-messages
+// Create web server
+var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var port = 3000;
+var comments = [];
+
+app.use(bodyParser.json());
+
+app.get('/comments', function(req, res) {
+  res.json(comments);
+});
+
+app.post('/comments', function(req, res) {
+  var comment = req.body;
+  comments.push(comment);
+  res.json(comment);
+});
+
+app.listen(port, function() {
+  console.log('Server is running on port', port);
+});
+```
+
+## 3.6.2. Testing with Postman
+
+###
